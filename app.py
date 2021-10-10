606431a8a84a8ca8a4157a1b78c374c2fbb73e00
nakashi94 <n0904yBUNJOU@gmail.com>
2021-10-10 14:17:19 +0900

    add style.css

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', \
+        title="Index with Jinja", \
+        message="これはJinjaテンプレートの利用例です！")
+
+@app.route('/<id>/<password>')
+def index2(id, password):
+    msg = 'id: %s, password: %s' % (id, password)
+    return render_template('index.html', \
+        title="Index with Jinja", \
+        message=msg)
 
 if __name__ == '__main__':
     app.debug = True
