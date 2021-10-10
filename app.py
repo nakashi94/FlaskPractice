ecdaea0b04c7be1d12016bbd5a7f70a57e540359
nakashi94 <n0904yBUNJOU@gmail.com>
2021-10-10 22:44:22 +0900

    jinja

--- app.py
+++ app.py
@@ -4,26 +4,28 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', \
-        title="Form sample", \
-        message="お名前は？")
+    flg = False
+    return render_template('jinja.html', \
+        title="Template sample", \
+        message="This is Jinja template sample.", \
+        flg=flg )
 
-@app.route('/', methods=['POST'])
-def form():
-    field = request.form['field']
-    ck = request.form.get('check')
-    rd = request.form.get('radio')
-    sel = request.form.getlist('sel')
-    return render_template('index.html', \
-        title="Form sample", \
-        message=[field, ck, rd, sel])
+# @app.route('/', methods=['POST'])
+# def form():
+    # field = request.form['field']
+    # ck = request.form.get('check')
+    # rd = request.form.get('radio')
+    # sel = request.form.getlist('sel')
+    # return render_template('index.html', \
+        # title="Form sample", \
+        # message=[field, ck, rd, sel])
 
-@app.route('/<id>/<password>')
-def index2(id, password):
-    msg = 'id: %s, password: %s' % (id, password)
-    return render_template('index.html', \
-        title="Index with Jinja", \
-        message=msg)
+# @app.route('/<id>/<password>')
+# def index2(id, password):
+    # msg = 'id: %s, password: %s' % (id, password)
+    # return render_template('index.html', \
+        # title="Index with Jinja", \
+        # message=msg)
 
 if __name__ == '__main__':
     app.debug = True
