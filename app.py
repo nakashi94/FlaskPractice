0deddd96776d5ae478cd2e7c7e0a2032d625c28a
nakashi94 <n0904yBUNJOU@gmail.com>
2021-10-11 09:44:44 +0900

    add layout page

--- app.py
+++ app.py
@@ -4,30 +4,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    data = ["One", "Two", "Three"]
-    person = {'name':'Taro', 'mail':'taro@yamada'}
-    return render_template('jinja.html', \
+    return render_template('index.html', \
         title="Template sample", \
-        message='This is sample message.', \
-        data=data, \
-        person=person )
-
-# @app.route('/', methods=['POST'])
-# def form():
-    # field = request.form['field']
-    # ck = request.form.get('check')
-    # rd = request.form.get('radio')
-    # sel = request.form.getlist('sel')
-    # return render_template('index.html', \
-        # title="Form sample", \
-        # message=[field, ck, rd, sel])
-
-# @app.route('/<id>/<password>')
-# def index2(id, password):
-    # msg = 'id: %s, password: %s' % (id, password)
-    # return render_template('index.html', \
-        # title="Index with Jinja", \
-        # message=msg)
+        message='This is sample page.' )
 
 if __name__ == '__main__':
     app.debug = True
