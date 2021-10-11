893efb79f483bf93264115360985ac343c51d993
nakashi94 <n0904yBUNJOU@gmail.com>
2021-10-11 13:13:13 +0900

    templateFilter

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
+    n = random.randrange(5, 10)
+    data = []
+    for N in range(n):
+        data.append(random.randrange(0, 100))
     return render_template('index.html', \
         title="Template sample", \
-        message='This is sample page.' )
+        message='※合計を計算する：', \
+        data=data )
 
 @app.route('/next', methods=['GET'])
 def next():
@@ -14,6 +20,15 @@ def next():
         title="Next page", \
         message="※これは、別のサンプルページです。", \
         data = ['One', 'Two', 'Three'] )
+
+@app.template_filter('sum')
+def sum_filter(data):
+    total = 0
+    for item in data:
+        total += item
+    return total
+
+app.jinja_env.filters['sum'] = sum_filter
 
 if __name__ == '__main__':
     app.debug = True
